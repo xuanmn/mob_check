50c95d49edcbe0fd1417f017923ea0852eb311e7
Xuan Nguyen <nguyemoc000@gmail.com>
2025-03-14 02:06:33 -0700

    ez inferno

--- src/test/java/com/mob_check/mobCheckPluginTest.java
+++ src/test/java/com/mob_check/mobCheckPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.mob_check;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class mobCheckPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(mobCheckPlugin.class);
 		RuneLite.main(args);
 	}
 }
