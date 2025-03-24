8385f09f715393b57943781ffc9b0d828fd605a9
Xuan Nguyen <nguyemoc000@gmail.com>
2025-03-24 12:38:12 -0700

    no errors

--- src/main/java/com/mob_check/MobCheckOverlay.java
+++ src/main/java/com/mob_check/MobCheckOverlay.java
@@ -1,10 +1,7 @@
 package com.mob_check;
 
-import net.runelite.client.util.Perspective;
 import net.runelite.api.Client;
 import net.runelite.api.NPC;
-import net.runelite.api.coords.LocalPoint;
-import net.runelite.api.Point;
 import net.runelite.client.ui.overlay.Overlay;
 import net.runelite.client.ui.overlay.OverlayLayer;
 import net.runelite.client.ui.overlay.OverlayPosition;
@@ -24,60 +21,33 @@ public class MobCheckOverlay extends Overlay
     {
         this.client = client;
         this.plugin = plugin;
-        setPosition(OverlayPosition.DYNAMIC);
-        setLayer(OverlayLayer.ABOVE_SCENE);
-        setPriority(OverlayPriority.HIGH);
+        setPosition(OverlayPosition.TOP_LEFT);
+        setLayer(OverlayLayer.ABOVE_WIDGETS);
+        setPriority(OverlayPriority.MED);
     }
 
     @Override
     public Dimension render(Graphics2D graphics)
     {
         Map<Integer, Integer> npcMap = plugin.getNpcNextAttackTickMap();
+        int y = 20;
+
+        graphics.setFont(new Font("Arial", Font.PLAIN, 14));
 
         for (NPC npc : client.getNpcs())
         {
-            if (!npcMap.containsKey(npc.getIndex()))
+            Integer ticks = npcMap.get(npc.getIndex());
+            if (ticks == null || ticks <= 0)
             {
                 continue;
             }
 
-            int ticks = npcMap.get(npc.getIndex());
-            if (ticks <= 0)
-            {
-                continue;
-            }
-
-            LocalPoint lp = npc.getLocalLocation();
-            if (lp == null)
-            {
-                continue;
-            }
-
-            Point textLocation = Perspective.getCanvasTextLocation(client, graphics, lp, npc.getLogicalHeight() + 40);
-            if (textLocation != null)
-            {
-                // Determine color based on ticks
-                Color tickColor;
-                if (ticks <= 1)
-                    tickColor = Color.RED;
-                else if (ticks <= 3)
-                    tickColor = Color.ORANGE;
-                else
-                    tickColor = Color.GREEN;
-
-                String tickText = String.valueOf(ticks);
-                graphics.setFont(new Font("Arial", Font.BOLD, 16));
-
-                // Shadow
-                graphics.setColor(Color.BLACK);
-                graphics.drawString(tickText, textLocation.getX() + 1, textLocation.getY() + 1);
-
-                // Foreground
-                graphics.setColor(tickColor);
-                graphics.drawString(tickText, textLocation.getX(), textLocation.getY());
-            }
+            String line = npc.getName() + " (" + npc.getIndex() + "): " + ticks + " ticks";
+            graphics.setColor(ticks <= 1 ? Color.RED : ticks <= 3 ? Color.ORANGE : Color.GREEN);
+            graphics.drawString(line, 10, y);
+            y += 16;
         }
 
-        return null;
+        return new Dimension(200, y);
     }
 }
