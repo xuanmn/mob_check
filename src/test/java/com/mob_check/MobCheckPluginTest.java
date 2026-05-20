16011d97792fca93db82f2c69e7aa2961c1a25eb
Xuan Nguyen <nguyemoc000@gmail.com>
2026-05-19 18:10:53 -0700

    style: reformat MobCheckPluginTest class declaration and brace style

--- src/test/java/com/mob_check/MobCheckPluginTest.java
+++ src/test/java/com/mob_check/MobCheckPluginTest.java
@@ -3,10 +3,8 @@ package com.mob_check;
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class MobCheckPluginTest
-{
-	public static void main(String[] args) throws Exception
-	{
+public class MobCheckPluginTest {
+	public static void main(String[] args) throws Exception {
 		ExternalPluginManager.loadBuiltin(MobCheckPlugin.class);
 		RuneLite.main(args);
 	}
